3c9b9ff199d0605d71c25366640919905bd3c840
Krystian <alazamaryn93@gmail.com>
2019-10-30 10:55:59 +0100

    task 2.3 done

--- development/js/app.js
+++ development/js/app.js
@@ -1,3 +1,5 @@
+// przyciski sidebar menu
+
 const btn1 =document.getElementById("nav-button1");
 const btn2 =document.getElementById("nav-button2");
 const btn3 =document.getElementById("nav-button3");
@@ -30,3 +32,25 @@ btn3.addEventListener("click", function() {
     btn1.querySelector("i").classList.remove("arrow");
 
 })
+
+//dodawanie name do localstore
+
+const newName =document.querySelector(".guest-name-input");
+const addName =document.querySelector(".guest-name-add");
+
+
+console.log("Masz na imię: ", localStorage.getItem("savedName"));
+if (localStorage.getItem("savedName")!==null) {
+    document.querySelector(".new-guest-content").classList.toggle("display-none");
+    document.querySelector(".header-right-name").innerText=localStorage.getItem("savedName");
+}
+
+
+addName.addEventListener("click", function () {
+    let userName = newName.value;
+    localStorage.setItem('savedName', userName);
+
+
+})
+
+
